9e428f7a694332c2b6c51fdc57f19bae645e1d88
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2024-05-14 10:59:40 -0300

    feat: created alunos CRUD

---
+++ src/controllers/AlunoController.js
@@ -0,0 +1,107 @@
+import Aluno from '../models/Aluno';
+
+class AlunoController {
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+    res.json(alunos);
+  }
+
+  async create(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.messsage),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.messsage),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      return res.json(alunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.messsage),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      await aluno.destroy();
+
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.messsage),
+      });
+    }
+  }
+}
+
+export default new AlunoController();
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Theylor',
-      sobrenome: 'Albuquerque',
-      email: 'theylor@gmail.com',
-      idade: 25,
-      peso: 82,
-      altura: 1.77,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
