7781c3082533ecc8244a59c6fd7264b639bdf477
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2024-11-21 21:43:46 -0300

    fix: liberada rota de registro

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,10 +5,10 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 // Não deveria existir
-router.get('/', loginRequired, userController.index); // Lista usuários
+// router.get('/', loginRequired, userController.index); // Lista usuários
 // router.get('/:id', userController.show); // Lista usuário
 
-router.post('/', loginRequired, userController.create);
+router.post('/', userController.create);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
 
